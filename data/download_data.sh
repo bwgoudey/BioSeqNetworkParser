597d782f850d88ae55e4560399d06a813f41d5fe
Benjamin Goudey <bgoudey@unimelb.edu.au>
2022-05-12 12:33:17 +1000

    Optimising genbank/refseq to swissprot mapping

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,1 +1,8 @@
-Data goes in this directory
+#!/bin/bash
+
+#wget https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.dat.gz
+#zcat < ./uniprot_sprot.dat.gz | bgzip -c > ./uniprot_sprot.dat.bgz
+
+for i in `seq 0 5 300`; do 
+    wget https://ftp.ncbi.nlm.nih.gov/genbank/gbbct${i}.seq.gz 
+done
